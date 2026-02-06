8d6046b55aca0e945168ec7148d0078664fecd76
tauhid2727 <169004644+tauhid2727@users.noreply.github.com>
2026-02-06 14:11:47 -0500

    Update send-lead-email.js

--- netlify/functions/send-lead-email.js
+++ netlify/functions/send-lead-email.js
@@ -25,7 +25,8 @@ exports.handler = async (event) => {
     // 🔴 THIS is the critical line — must run
     const email = await resend.emails.send({
       from: "Mohammad AI <onboarding@resend.dev>", // SAFE TEST SENDER
-      to: ["hello@mohammadaisolutions.com"],  cc: ["tauhid27@gmail.com"],
+      to: ["hello@mohammadaisolutions.com"],  
+      cc: ["tauhid27@gmail.com"],
                 // your inbox
       subject: "🔥 New Lead Test",
       html: `
